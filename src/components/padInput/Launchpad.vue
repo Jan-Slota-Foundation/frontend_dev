b0ff8afe318404843ad1d8cdd977a9d9ba0e2eb2
SIPOS Filip SK79A <filip.sipos@student.leaf.academy>
2024-04-06 20:10:48 +0200

    button for pushing

--- src/components/padInput/Launchpad.vue
+++ src/components/padInput/Launchpad.vue
@@ -2,9 +2,9 @@
 import { lpSequence } from '@/states/launchpadSequence.js'
 </script>
 <template>
-  <div :class="`grid grid-cols-${5} gap-4 w-[35rem] mb-8`">
+  <div :class="`grid grid-cols-5 gap-4 w-[35rem] mb-8 `">
     <button
-      class="bg-zinc-800 text-xl text-zinc-400 hover:shadow-md border border-zinc-700 hover:shadow-rose-700 rounded-xl aspect-square transition-all duration-150 ease-out active:border-rose-600 active:bg-rose-600 outline-none"
+      class="bg-zinc-800 w-full text-xl text-zinc-400 hover:shadow-md border border-zinc-700 hover:shadow-rose-700 rounded-xl aspect-square transition-all duration-150 ease-out active:border-rose-600 active:bg-rose-600 outline-none"
       @click="handleLaunchpadButtonClick(note)"
       v-for="(note, index) in notesMapped"
       :key="index"
@@ -13,7 +13,7 @@ import { lpSequence } from '@/states/launchpadSequence.js'
     </button>
     <button
       @click="handleLaunchpadButtonClick({ name: '-' })"
-      class="bg-zinc-800 row-start-1 text-zinc-400 col-start-5 row-span-4 hover:shadow-md border border-zinc-700 hover:shadow-rose-700 rounded-xl transition-all duration-150 ease-out active:border-rose-600 active:bg-rose-600 outline-none"
+      class="bg-zinc-800 row-start-1 text-zinc-400 col-start-5 row-span-3 hover:shadow-md border border-zinc-700 hover:shadow-rose-700 rounded-xl transition-all duration-150 ease-out active:border-rose-600 active:bg-rose-600 outline-none"
     >
       -
     </button>
@@ -25,7 +25,7 @@ export default {
   name: 'Launchpad',
   data() {
     return {
-      notes: ['c', 'd', 'e', 'f', 'g', 'a', 'h', 'C']
+      notes: ['c', 'd', 'e', 'f', 'g', 'a', 'b']
     }
   },
   methods: {
@@ -45,9 +45,12 @@ export default {
         final.push({
           name: `${note}`
         })
-        final.push({
-          name: `${note}#`
-        })
+        if (note !== 'b' && note !== 'e') {
+          console.log(note)
+          final.push({
+            name: `${note}#`
+          })
+        }
       })
       return final
     }
--- src/components/padInput/ScrollSequence.vue
+++ src/components/padInput/ScrollSequence.vue
@@ -2,35 +2,64 @@
 import { lpSequence } from '@/states/launchpadSequence.js'
 </script>
 <template>
-  <transition-group
-    tag="div"
-    name="list"
-    class="flex flex-row-reverse gap-2 bg-zinc-950 p-2 rounded-xl w-[38rem] h-[3.6rem] overflow-x-scroll overflow-y-hidden container"
-  >
-    <div
-      :class="[
-        '-z-[3px] py-2 px-4 bg-zinc-800 rounded-md min-w-[5rem] overflow-hidden flex items-center justify-center border ',
-        note.id === lpSequence.numberOfItems - 1
-          ? 'border-rose-700'
-          : 'border-zinc-700'
-      ]"
-      v-for="note in lpSequence.array"
-      :key="note.id"
+  <div class="flex gap-12">
+    <transition-group
+      tag="div"
+      name="list"
+      class="flex flex-row-reverse gap-2 bg-zinc-950 p-2 rounded-xl w-[38rem] h-[3.6rem] overflow-x-scroll overflow-y-hidden container"
     >
-      {{ note.name.split().reverse().join() }}
-    </div>
-  </transition-group>
+      <div
+        :class="[
+          '-z-[3px] py-2 px-4 bg-zinc-800 rounded-md min-w-[5rem] overflow-hidden flex items-center justify-center border ',
+          note.id === lpSequence.numberOfItems - 1
+            ? 'border-rose-700'
+            : 'border-zinc-700'
+        ]"
+        v-for="note in lpSequence.array"
+        :key="note.id"
+      >
+        {{ note.name.split().reverse().join() }}
+      </div>
+    </transition-group>
+    <button
+      @click="handlePush"
+      class="bg-rose-700 px-6 rounded-lg tracking-wider font-semibold"
+    >
+      Push
+    </button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'ScrollSequence'
+  name: 'ScrollSequence',
+  methods: {
+    async handlePush() {
+      try {
+        await port.open({ baudRate: 9600 })
+
+        const encoder = new TextEncoder()
+
+        const writer = port.writable.getWriter()
+        try {
+          await writer.write(8)
+        } catch (error) {
+          console.error(error)
+        }
+
+        writer.releaseLock()
+      } catch (error) {
+        console.error(error)
+      }
+    }
+  }
 }
 </script>
 
 <style scoped>
 .container {
-  /* direction: rtl; */
+  scrollbar-color: #09090b #27272a;
+  scrollbar-width: thin;
 }
 
 .list-enter-active,
@@ -41,5 +70,9 @@ export default {
 .list-leave-to {
   opacity: 0;
   transform: translateY(30px);
+}
+
+::-webkit-scrollbar {
+  display: none;
 }
 </style>
