42d6204fde0b5840e2ea4ade1c748833c7d2a673
Adam Bíša <117039930+adambisa@users.noreply.github.com>
2024-04-06 12:16:31 +0200

    grid

--- src/components/Launchpad.vue
+++ src/components/Launchpad.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>Launchpad component</div>
+  <div class="grid grid-cols-4">
+
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+    <button>4</button>
+
+    <button>5</button>
+    <button>6</button>
+    <button>7</button>
+    <button>8</button>
+
+    <button>9</button>
+    <button>10</button>
+    <button>11</button>
+    <button>12</button>
+
+    <button>13</button>
+    <button>14</button>
+    <button>15</button>
+    <button>16</button>
+
+
+    
+  </div>
+
 </template>
 
 <script>
@@ -8,4 +33,6 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+
+</style>
