5103f13dd305be3d3299126986a95a74f20115fc
Adam Bíša <117039930+adambisa@users.noreply.github.com>
2024-04-06 22:37:07 +0200

    text field

--- src/components/textInput/TextField.vue
+++ src/components/textInput/TextField.vue
@@ -1,24 +1,25 @@
 <template>
-  <div
-    class="bg-zinc-800 w-[25rem] h-[15rem] rounded-xl flex justify-center flex-col items-center"
-  >
-    <div>Selected: {{ selected }}</div>
+  <div class="flex flex-col items-end">
+    <div
+      class="bg-zinc-800 w-[25rem] h-[18rem] rounded-xl flex justify-around flex-col items-center"
+    >
+      <div>Selected voice: {{ selected }}</div>
 
-    <select class="bg-zinc-700 rounded-lg pb-10" v-model="selected">
-      <option disabled value="">Please select one</option>
-      <option>MALE</option>
-      <option>FEMALE</option>
-    </select>
-    <p style="white-space: pre-line">{{ message }}</p>
-    <textarea
-      class="outline-none resize-none rounded-xl bg-zinc-700 w-[20rem] h-[10rem] pl-5 pt-2"
-      v-model="message"
-      placeholder="Type here to make our computer speak your mind..."
-      maxlength="255"
-    ></textarea>
+      <select class="bg-zinc-700 rounded-xl w-[15rem] pl-2" v-model="selected">
+        <option disabled value="">Please select one</option>
+        <option>MALE</option>
+        <option>FEMALE</option>
+      </select>
+      <textarea
+        class="outline-none resize-none rounded-xl bg-zinc-700 w-[20rem] h-[10rem] pl-5 pt-2"
+        v-model="message"
+        placeholder="Type here to make our computer speak your mind..."
+        maxlength="255"
+      ></textarea>
+    </div>
+    <button class="bg-rose-600 rounded-xl w-[10rem]">SEND MESSAGE</button>
   </div>
 </template>
-
 <script>
 export default {
   name: 'TextField',
