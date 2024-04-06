55e7f7543b78db0611e87da04ecc427608f777cf
Adam Bíša <117039930+adambisa@users.noreply.github.com>
2024-04-07 00:32:57 +0200

    log updt

--- src/views/LoginForm.vue
+++ src/views/LoginForm.vue
@@ -1,9 +1,38 @@
+<script setup>
+import router from '@/router/index.js'
+</script>
+
 <template>
-  <div>log</div>
+  <div class="flex items-center pt-10 flex-col">
+    <div
+      class="bg-zinc-700 w-[28rem] h-[42rem] flex justify-center items-center flex-col rounded-xl pt-7 pb-3"
+    >
+      <h1>LOGIN NOW</h1>
+      <input class="h-[2rem]" v-model="email" placeholder="Enter your email" />
+      <input
+        class="h-[2rem]"
+        v-model="password"
+        placeholder="Enter your password"
+      />
+      <button @click="login()">login</button>
+      <router-link to="/register">I wish to register</router-link>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  // v for pre udaje
+  name: 'RegisterForm',
+  methods: {},
+  //do data
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  }
+}
 </script>
 
 <style scoped></style>
