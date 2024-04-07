2e755b033053c0c539b1a9eff49ed88fe2723482
SIPOS Filip SK79A <filip.sipos@student.leaf.academy>
2024-04-07 10:22:51 +0200

    text@voice

--- src/components/textInput/TextField.vue
+++ src/components/textInput/TextField.vue
@@ -2,26 +2,18 @@
 import { serial } from '@/states/serialStates.js'
 </script>
 <template>
-  <div class="flex flex-col items-end">
-    <div
-      class="bg-zinc-800 w-[25rem] h-[18rem] rounded-xl flex justify-around flex-col items-center"
+  <div class="flex flex-col gap-[2rem] mt-24 items-center">
+    <textarea
+      class="outline-none resize-none rounded-xl bg-zinc-800 w-[32rem] h-[18rem] px-5 pt-3 border border-zinc-600"
+      v-model="message"
+      placeholder="Type here to make our computer speak your mind..."
+      maxlength="255"
+    ></textarea>
+    <button
+      @click="handleSendText"
+      class="bg-rose-700 text-lg rounded-lg px-6 py-2 hover:bg-rose-800 font-semibold tracking-wider"
     >
-      <div>Selected voice: {{ selected }}</div>
-
-      <select class="bg-zinc-700 rounded-xl w-[15rem] pl-2" v-model="selected">
-        <option disabled value="">Please select one</option>
-        <option>MALE</option>
-        <option>FEMALE</option>
-      </select>
-      <textarea
-        class="outline-none resize-none rounded-xl bg-zinc-700 w-[20rem] h-[10rem] pl-5 pt-2"
-        v-model="message"
-        placeholder="Type here to make our computer speak your mind..."
-        maxlength="255"
-      ></textarea>
-    </div>
-    <button @click="handleSendText" class="bg-rose-600 rounded-xl w-[10rem]">
-      Transcribe to morse
+      Transcribe
     </button>
   </div>
 </template>
